ef4e520d181d9fa8dbfd826ba8c54beadb64fb93
Huang-Huang Bao <eh5@sokka.cn>
2022-04-10 23:00:40 +0800

    packages: fake-hwclock: use coreutils

--- packages/fake-hwclock/default.nix
+++ packages/fake-hwclock/default.nix
@@ -1,5 +1,8 @@
-{ lib, writeShellScriptBin }:
-writeShellScriptBin "fake-hwclock" (builtins.readFile ./fake-hwclock.sh) // {
+{ lib, writeShellScriptBin, coreutils }:
+writeShellScriptBin "fake-hwclock" ''
+  export PATH="$PATH''${PATH:+:}${coreutils}/bin"
+  ${builtins.readFile ./fake-hwclock.sh}
+'' // {
   meta = {
     description = "Fake hardware clock";
     license = lib.licenses.gpl3;
